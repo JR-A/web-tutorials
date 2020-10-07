cee5ff258cd14f29ad36db9792b3ceb786829f6c
jrAhn <kn043143@naver.com>
2020-10-07 17:42:16 +0900

    add JavaBean used in JSTL

---
+++ jspMain/src/com/member/Member.java
@@ -0,0 +1,12 @@
+package com.member;
+
+public class Member {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
